92c6519c0d8cbf5b789cf1c219b72a662532ba41
Guilherme H. Marques Cardoso <87186496+GuilhermeHMC@users.noreply.github.com>
2026-03-02 22:32:43 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
 function Home() {
-    return <h1>Helena, Estuda ai e me liga mais tarde quando puder, também estou estudando aqui. Olha o site que estou fazendo pra te mosrar depois :) ☃️</h1>
+  return (
+    <h1>
+      Helena, Estuda ai e me liga mais tarde quando puder, também estou
+      estudando aqui. Olha o site que estou fazendo pra te mostrar depois :) ☃️
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("identação errada");
 }
 
 export default Home;
