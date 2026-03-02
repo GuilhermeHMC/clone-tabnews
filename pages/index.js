d29a092e8b8c6f1e94f822e286e7dbac99859f57
Guilherme H. Marques Cardoso <87186496+GuilhermeHMC@users.noreply.github.com>
2026-03-02 18:02:45 +0000

    modifica a frase pra Helena

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Under Construction... :) ☃️</h1>
+    return <h1>Helena, Estuda ai e me liga mais tarde quando puder, também estou estudando aqui. Olha o site que estou fazendo pra te mosrar depois :) ☃️</h1>
 }
 
 export default Home;
